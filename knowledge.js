5a86dddb5e1811abed5e4bdc1d1cbecfd274eeb2
Tiju Abraham <142086525+TijuAbraham1@users.noreply.github.com>
2026-01-02 23:37:09 -0800

    Update knowledge.js

--- knowledge.js
+++ knowledge.js
@@ -2,7 +2,7 @@
 export const knowledgeBase = [
     { id: 1, section: "Executive Summary", text: "TIJU ABRAHAM"},
     { id: 2, section: "Experience, Education and Integrations", text: "Roles Managed"},
-    { id: 3, section: "Project Details", text: "Deep experience in multi-cloud strategies using AWS and Azure for large-scale IT delivery." },
+    { id: 3, section: "Project Details", text: "Deep experience in multi-cloud strategies using AWS and Azure for large-scale IT delivery." }
   
     // Continue adding chunks for all 17 pages here...
 ];
